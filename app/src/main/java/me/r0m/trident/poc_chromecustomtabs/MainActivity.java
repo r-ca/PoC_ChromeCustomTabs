433cffcaa28061654893bc973122a40c36834d12
ろむねこ <nem@nem.x0.to>
2024-01-11 11:25:21 +0900

    WIP ロジック実装

--- app/src/main/java/me/r0m/trident/poc_chromecustomtabs/MainActivity.java
+++ app/src/main/java/me/r0m/trident/poc_chromecustomtabs/MainActivity.java
@@ -1,13 +1,31 @@
 package me.r0m.trident.poc_chromecustomtabs;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText urlInput = findViewById(R.id.url_input);
+        Button openUrlButton = findViewById(R.id.open_url_button);
+        openUrlButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = urlInput.getText().toString();
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                CustomTabsIntent customTabsIntent = builder.build();
+                customTabsIntent.launchUrl(MainActivity.this, Uri.parse(url));
+            }
+        });
     }
 }
